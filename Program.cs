8dda3f95a32117d81bc2a599b08c3e716d2c60ea
PrestonW22 <prestondeanwaters@gmail.com>
2025-09-03 21:37:10 -0600

    finished step 6

--- Program.cs
+++ Program.cs
@@ -1,5 +1,15 @@
-﻿int max = int.MaxValue;
-int min = int.MinValue;
-Console.WriteLine($"The range of integers is {min} to {max}");
-int what = max + 3;
-Console.WriteLine($"An example of overflow: {what}");
+﻿double a = 19;
+double b = 23;
+double c = 8;
+double d = (a + b) / c;
+Console.WriteLine(d);
+
+double max = double.MaxValue;
+double min = double.MinValue;
+Console.WriteLine($"The range of double is {min} to {max}");
+
+double third = 1.0 / 3.0;
+Console.WriteLine(third);
+
+double weird = 1.0 / 34;
+Console.WriteLine(weird - 34.567);
