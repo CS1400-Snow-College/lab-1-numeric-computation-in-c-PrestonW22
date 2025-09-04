3608a498cef674d86b8026402946771f72298e37
PrestonW22 <prestondeanwaters@gmail.com>
2025-09-03 21:18:49 -0600

    finished part 3

--- Program.cs
+++ Program.cs
@@ -1,9 +1,5 @@
-﻿int a = 5;
+﻿int a = 7;
 int b = 4;
-int c = 2;
-int d = a + b * c;
-int e = (a + b) * c;
-int f = (a + b) * c - 1 / 2;
-Console.WriteLine(f);
+int c = 3;
+int d = (a + b) / c;
 Console.WriteLine(d);
-Console.WriteLine(e);
--- obj/Debug/net9.0/lab-1-numeric-computation-in-c-PrestonW22.AssemblyInfo.cs
+++ obj/Debug/net9.0/lab-1-numeric-computation-in-c-PrestonW22.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab-1-numeric-computation-in-c-PrestonW22")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8236f9a2257140b3ae70cf5084b6a725f1b7e825")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ebb87c18f2a89b1f39ac1a2d1c1e607a3cb59e0d")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab-1-numeric-computation-in-c-PrestonW22")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab-1-numeric-computation-in-c-PrestonW22")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
