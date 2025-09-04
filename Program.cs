2f721b431c6224e36d27171c74febb4f59de43db
PrestonW22 <prestondeanwaters@gmail.com>
2025-09-03 21:31:43 -0600

    finished step 5

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
-﻿int a = 7;
-int b = 4;
-int c = 3;
-int d = (a + b) / c;
-int e = (a + b) % c;
-Console.WriteLine(d);
-Console.WriteLine(e);
+﻿int max = int.MaxValue;
+int min = int.MinValue;
+Console.WriteLine($"The range of integers is {min} to {max}");
+int what = max + 3;
+Console.WriteLine($"An example of overflow: {what}");
